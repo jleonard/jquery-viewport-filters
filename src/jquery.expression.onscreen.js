3dbc532211deb2cb1a0ef2fe71583e7ae9da97eb
jleonard <thejohnleonard@gmail.com>
2014-11-12 17:28:14 -0500

    adding test

--- src/jquery.expression.onscreen.js
+++ src/jquery.expression.onscreen.js
@@ -10,26 +10,25 @@
 
       var top = $(element).offset().top;
       var pixelsOnScreen = bottomOfViewport - top;
-      var percentOnScreen = pixelsOnScreen / $(element).outerHeight();
+      var percentOnScreen = ( pixelsOnScreen / $(element).outerHeight() ) * 100;
       console.log(pixelsOnScreen,percentOnScreen);
-
-      if(percentOnScreen >= num){
-        console.log()
-        console.log(element);
-        console.log('pixelsOnScreen ',pixelsOnScreen +'px');
-        console.log('percentOnScreen ',percentOnScreen +'%'); 
-        console.log('eval against ',num);
-      }
 
       return  percentOnScreen >= num;
 
+    },
+
+    pixelsOnScreen: function( element, index, details, collection ){
+
+      var num = parseInt(details[3],10);
+      var bottomOfViewport = $(window).height() + $(window).scrollTop();
+
+      var top = $(element).offset().top;
+      var pixelsOnScreen = bottomOfViewport - top;
+
+      return  pixelsOnScreen >= num;
+
     }
+
   });
 
 })(window.jQuery);
-
-$(window).on('scroll',function(e){
-  var o = $('.cube:percentOnScreen(25)');
-  console.log('\n');
-  //console.log(o);
-});
