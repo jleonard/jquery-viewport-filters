aac508b7f9e2e0c548df7834e005b8a7fb3d3173
jleonard <thejohnleonard@gmail.com>
2014-11-13 12:23:47 -0500

    adding more filters

--- src/jquery.expression.onscreen.js
+++ src/jquery.expression.onscreen.js
@@ -1,32 +1,85 @@
 
 (function($){
 
+  $.aboveViewport = function(element){
+    var topOfViewPort = $(window).scrollTop();
+    var top = $(element).offset().top;
+    return top <= topOfViewPort;
+  }
+
+  $.belowViewport = function(element){
+    var bottomOfViewport = $(window).height() + $(window).scrollTop();
+    var top = $(element).offset().top;
+    return (top + $(element).outerHeight() ) >= bottomOfViewport;
+  };
+
+  $.insideViewport = function(element){
+    return !$.aboveViewport(element) && !$.belowViewport(element) && !$.offscreenBottom(element) && !$.offscreenTop(element);
+  };
+
+  $.offscreenBottom = function(element){
+    var bottomOfViewport = $(window).height() + $(window).scrollTop();
+    var top = $(element).offset().top;
+    return top >= bottomOfViewport;
+  };
+
+  $.offscreenTop = function(element){
+    var topOfViewPort = $(window).scrollTop();
+    var top = $(element).offset().top;
+    return (top + $(element).outerHeight() ) <= topOfViewPort;
+  };
+
   $.extend($.expr[":"], {
 
+    insideViewport : function(element, index, details, collection){
+      return $.inViewport(element);
+    },
+
+    aboveViewport : function(element, index, details, collection){
+      return $.aboveViewport(element);
+    },
+
+    belowViewport : function(element, index, details, collection){
+      return $.belowViewport(element);
+    },  
+
     percentOnScreen: function( element, index, details, collection ){
-
       var num = parseInt(details[3],10);
+      var percentOnScreen = 100;
+      var pixelsOnScreen = $(element).outerHeight();
+      var topOfViewPort = $(window).scrollTop();
       var bottomOfViewport = $(window).height() + $(window).scrollTop();
-
       var top = $(element).offset().top;
-      var pixelsOnScreen = bottomOfViewport - top;
-      var percentOnScreen = ( pixelsOnScreen / $(element).outerHeight() ) * 100;
-      console.log(pixelsOnScreen,percentOnScreen);
 
-      return  percentOnScreen >= num;
+      if($.belowViewport(element)){
+        pixelsOnScreen = bottomOfViewport - top;
+      }
 
+      if($.aboveViewport(element)){
+        pixelsOnScreen = Math.abs( topOfViewPort - (top + $(element).outerHeight()) );
+      }
+     
+      percentOnScreen = ( pixelsOnScreen / $(element).outerHeight() ) * 100;
+
+      return  percentOnScreen >= num && percentOnScreen < 101;
     },
 
     pixelsOnScreen: function( element, index, details, collection ){
-
       var num = parseInt(details[3],10);
+      var pixelsOnScreen = $(element).outerHeight();
+      var topOfViewPort = $(window).scrollTop();
       var bottomOfViewport = $(window).height() + $(window).scrollTop();
-
       var top = $(element).offset().top;
-      var pixelsOnScreen = bottomOfViewport - top;
+
+      if($.belowViewport(element)){
+        pixelsOnScreen = bottomOfViewport - top;
+      }
+
+      if($.aboveViewport(element)){
+        pixelsOnScreen = ( topOfViewPort - (top + $(element).outerHeight()) ) * -1;
+      }
 
       return  pixelsOnScreen >= num;
-
     }
 
   });
